2ceb544a0b18ed093473edbafe9af1dfcc9a36cc
Robert Northard <robertnorthard@googlemail.com>
2013-12-27 14:52:57 +0000

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,27 +1,32 @@
 /*
-* A simple jQuery image slider
-*/
-
+ * A simple jQuery image slider
+ */
 $(document).ready(startCounter);
 
-function startCounter(){
+function startCounter() {
 
-	var noImage = $('.slider > img').size();
-	var count = 0;
+    var noImage = $('.slider > img').size();
+    var count = 0;
 
-	$('.slider > img#img' + count).show('slide', {direction : 'left'}, 700);
+    $('.slider > img#img' + count).show('slide', {
+        direction: 'left'
+    }, 700);
 
-	var time = setInterval(function(){
-			$('.slider > img#img' + count).hide('slide', {direction : 'right'}, 400);
-			next();
-			$('.slider > img#img' + count).show('slide', {direction : 'left'}, 700);
-	}, 3000);
+    var time = setInterval(function () {
+        $('.slider > img#img' + count).hide('slide', {
+            direction: 'right'
+        }, 400);
+        next();
+        $('.slider > img#img' + count).show('slide', {
+            direction: 'left'
+        }, 700);
+    }, 3000);
 
-	function next(){
-		count = (count + 1) % noImage;
-	}
+    function next() {
+        count = (count + 1) % noImage;
+    }
 
-	function previous(){
-		count = (count - 1) % noImage;
-	}
+    function previous() {
+        count = (count - 1) % noImage;
+    }
 }
